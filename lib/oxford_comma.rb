1a4c8e5e057452947a5bf3ff78dd5d06dbd8fad3
Chris Nady <nady.christopher@gmail.com>
2017-09-06 14:40:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,8 @@ def oxford_comma(array)
   array.each_with_index do |i,index|
     if index==0
       new_string<<i
+    elsif i=-array.last && array.length ==2
+      new_string<<" and #{i}"
     elsif i==array.last
       new_string<<", and #{i}"
     else
