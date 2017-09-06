9ade6bbc9f533d075fdef178da2603e3c76f2101
Chris Nady <nady.christopher@gmail.com>
2017-09-06 14:32:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,11 @@
 def oxford_comma(array)
   new_string = String.new
-  array.each do |i|
-    new_string<<i
+  array.each_with_index do |i,index|
+    if i==0
+      new_string<<i
+    else
+      new_string<<" and #{i}"
+    end
   end
   new_string
 end
